d13f427442bd1f6e779dbb3795825ae5aaea36bb
MaheshN1821 <maheshmahi18042004@gmail.com>
2026-01-09 21:01:17 +0530

    cors policy updated

--- server/api/server.js
+++ server/api/server.js
@@ -14,19 +14,6 @@ connectDB();
 
 const app = express();
 
-app.use((req, res, next) => {
-	res.header(
-		"Access-Control-Allow-Origin",
-		"https://task-drab-seven.vercel.app"
-	);
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Specify allowed methods
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-	);
-	next();
-});
-
 const corsOptions = {
 	origin: "https://task-drab-seven.vercel.app",
 	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
